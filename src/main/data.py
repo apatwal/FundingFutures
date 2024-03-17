a4bb5d7e072c44415f39e65b849d95a6eee5da24
Aditya Patwal <adityapatwal07@gmail.com>
2024-03-16 20:49:01 -0700

    Add different filters

--- src/main/data.py
+++ src/main/data.py
@@ -1,50 +1,104 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import requests
 
+# api_key = '68ca08d738dd1219a5f92c9d16b60c65'
+# app_id = '5110e220'
+# url = 'https://api.schooldigger.com/v2.0/districts'
+#
+# params = {
+#     'q': 'Liberty',
+#     'st': 'CA',
+#     'appID': app_id,
+#     'appKey': api_key,
+# }
+# response = requests.get(url, params=params)
+#
+# if response.status_code == 200:
+#     # Parse the JSON response into a Python dictionary
+#     data = response.json()
+#     for school in data['districtList']:
+#         print(school['districtName'])
+#     print(data)
+st.title("Underfunded District Identifier")
+st.subheader("2020 California School Districts Information")
 
-# bigdf = pd.read_csv("/Users/adityapatwal/Documents/hackathon/districtcosts/Data-Table 1.csv")
-# california_df = bigdf[bigdf['state_name'] == 'California']
-# df = california_df[california_df['year'] == 2020]
-# new_df = df[['year', 'district', 'ppcstot', 'predcost', 'fundinggap', 'enroll']]
-# negative_fundinggaps_df = new_df[new_df['fundinggap'] < 0]
-# negative_fundinggaps_df = negative_fundinggaps_df.copy()
-# negative_fundinggaps_df.loc[:, 'totalgap'] = negative_fundinggaps_df['fundinggap'] * negative_fundinggaps_df['enroll']
-# lowest_totalgaps = negative_fundinggaps_df.nlargest(100, 'totalgap', keep='last').sort_values('totalgap')
-# print(lowest_totalgaps)
-#
-#
-# st.title("Underfunded school identifier")
+opt0 = 'Drop Down Menu'
+opt1 = 'Impacting a greater audience'
+opt2 = 'Helping those in need equally'
+option = st.selectbox('What is most important to you?',
+     (opt0, opt1, opt2))
+
 
 #create dataframe of 100 schools with least funding
 original_df = pd.read_csv("/Users/adityapatwal/Documents/hackathon/districtcosts/Data-Table 1.csv")
+
+#filter to data from California 2020
 california_df = original_df[original_df['state_name'] == 'California']
 temp_df = california_df[california_df['year'] == 2020]
-new_df = temp_df[['year', 'district', 'ppcstot', 'predcost', 'fundinggap', 'enroll']]
-negative_fundinggaps_df = new_df[new_df['fundinggap'] < 0]
-negative_fundinggaps_df = negative_fundinggaps_df.copy()
-negative_fundinggaps_df.loc[:, 'totalgap'] = negative_fundinggaps_df['fundinggap'] * negative_fundinggaps_df['enroll']
-totalgap_sum = negative_fundinggaps_df['totalgap'].sum()
-negative_fundinggaps_df.loc[:, 'weight'] = np.sqrt(negative_fundinggaps_df['totalgap']/totalgap_sum)
-lowest_totalgaps = negative_fundinggaps_df.nlargest(100, 'weight', keep='last').sort_values('weight')
 
-#calculate what percentage of funds should be allocated to each school
-weight_sum = lowest_totalgaps['weight'].sum()
-lowest_totalgaps['weight'] = (lowest_totalgaps['weight']/weight_sum)
-#st.write("The sum is ", lowest_totalgaps['weight'].sum())
+# only consider negative funding gaps per pupil
+new_df = temp_df[['district', 'ppcstot', 'predcost', 'fundinggap', 'enroll']]
+negative_fundinggaps_df = new_df[new_df['fundinggap'] < 0]
+
+#calculate totalGaps for each school
+negative_fundinggaps_df = negative_fundinggaps_df.copy()
+negative_fundinggaps_df.loc[:, 'Total Gap'] = negative_fundinggaps_df['fundinggap'] * negative_fundinggaps_df['enroll']
+totalgap_sum = negative_fundinggaps_df['Total Gap'].sum()
+
+negative_fundinggaps_df = negative_fundinggaps_df.rename(columns = {'district':'District',
+                                                                    'ppcstot': 'Spending per Pupil',
+                                                                    'predcost' : 'Required Spending',
+                                                                    'fundinggap' : 'Funding Gap',
+                                                                    'enroll' : 'Enrollment Size',
+                                                                    })
+totalEnrollment = negative_fundinggaps_df['Enrollment Size'].sum()
+
+st.dataframe(negative_fundinggaps_df)
+
+
+# WEIGHT
 
 funds = int(st.text_input(label="Enter Funds...", value="0"))
 st.write("The amount of funds is equal to ", funds)
-lowest_totalgaps.loc[:, 'fundsalloc'] = lowest_totalgaps['weight'] * funds
 
-lowest_totalgaps = lowest_totalgaps[['district', 'fundinggap', 'totalgap', 'fundsalloc']]
-lowest_totalgaps.rename(columns={"district": "District", "fundinggap": "Per Student Funding Gap", "totalgap": "Total Funding Gap", "fundsalloc": "Total Allocated Funds"})
-#['District', 'Per Student Funding Gap', 'Total Funding Gap', 'Total Allocated Funds']
+
+if option == opt1:
+    # find weight by taking sqrt of a schools total gap / California's funding gap
+    negative_fundinggaps_df.loc[:, 'weight'] = np.sqrt(negative_fundinggaps_df['Total Gap']/totalgap_sum)
+    lowest_totalgaps = negative_fundinggaps_df.nlargest(100, 'weight', keep='last').sort_values('weight')
+
+    # calculate what percentage of funds should be allocated to each school
+    weight_sum = lowest_totalgaps['weight'].sum()
+    lowest_totalgaps['weight'] = (lowest_totalgaps['weight']/weight_sum)
+
+
+    lowest_totalgaps.loc[:, 'Total Allocated Funds'] = lowest_totalgaps['weight'] * funds
+
+    lowest_totalgaps = lowest_totalgaps[['District', 'Funding Gap', 'Total Gap', 'Total Allocated Funds']]
+    lowest_totalgaps = lowest_totalgaps.rename(columns={"Funding Gap": "Per Student Funding Gap", "Total Gap": "Total Funding Gap"})
+
+elif option == opt2:
+     negative_fundinggaps_df.loc[:, 'weight'] = np.sqrt(negative_fundinggaps_df['Enrollment Size']/totalEnrollment)
+     lowest_totalgaps = negative_fundinggaps_df.nlargest(100, 'weight', keep='last').sort_values('weight')
+     # calculate what percentage of funds should be allocated to each school
+     weight_sum = lowest_totalgaps['weight'].sum()
+     lowest_totalgaps['weight'] = (lowest_totalgaps['weight']/weight_sum)
+     lowest_totalgaps.loc[:, 'Total Allocated Funds'] = lowest_totalgaps['weight'] * funds
+
+     lowest_totalgaps = lowest_totalgaps[['District', 'Funding Gap', 'Total Gap', 'Total Allocated Funds']]
+     lowest_totalgaps = lowest_totalgaps.rename(columns={"Funding Gap": "Per Student Funding Gap", "Total Gap": "Total Funding Gap"})
+
+
+
+
 
 
 #Display information
-st.title("Underfunded School Identifier")
-st.dataframe(lowest_totalgaps)
+if option == opt2 or option == opt1:
+    st.dataframe(lowest_totalgaps)
+
 
 
 
